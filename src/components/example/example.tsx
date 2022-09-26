970273dff63d6ad273c3ab94fbe6bda2cabdbdff
CircleCI Deploy Script <edward.prado@weareadaptive.com>
2022-09-26 11:57:00 -0400

    feat: add example hello world component

---
+++ src/components/example/example.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {
+  text?: string;
+}
+
+export default function example(props: Props) {
+  return <div>Hello {props.text ? props.text : "World"}</div>;
+}
