49f6f91e29ca5607c85edf20a9bba5cc9045079d
Wenfeng Xing <wenfeng.xing@weareadaptivee.com>
2022-09-27 15:18:41 -0400

    style: format the story

--- src/components/example/example.stories.tsx
+++ src/components/example/example.stories.tsx
@@ -1,19 +1,17 @@
-import React from "react";
-import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from 'react';
 
-import Example from "./example";
+import Example from './example';
 
 export default {
-  title: "Components/Example",
+  title: 'Components/Example',
   component: Example,
 } as ComponentMeta<typeof Example>;
 
-const Template: ComponentStory<typeof Example> = (args) => (
-  <Example {...args} />
-);
+const Template: ComponentStory<typeof Example> = (args) => <Example {...args} />;
 
 export const Basic = Template.bind({});
 
 Basic.args = {
-  text: "",
+  text: '',
 };
