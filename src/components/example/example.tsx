0be5c87e62bd2666327025f2707cb1875b047ff7
Wenfeng Xing <wenfeng.xing@weareadaptivee.com>
2022-09-27 12:58:42 -0400

    style: format

--- src/components/example/example.tsx
+++ src/components/example/example.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
 interface Props {
   text?: string;
 }
 
 export default function example(props: Props) {
-  return <div>Hello {props.text ? props.text : "World"}</div>;
+  return <div>Hello {props.text ? props.text : 'World'}</div>;
 }
