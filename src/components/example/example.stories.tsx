7a596f4e2f6baecfe00458db6ad2a29384fcf0b6
CircleCI Deploy Script <edward.prado@weareadaptive.com>
2022-10-04 11:08:36 -0400

    feat: add accessibility story examples

--- src/components/example/example.stories.tsx
+++ src/components/example/example.stories.tsx
@@ -10,5 +10,16 @@ export default {
 const Template: ComponentStory<typeof Example> = (args) => <Example {...args} />;
 
 export const HelloWorld = Template.bind({});
-
 HelloWorld.args = {};
+
+export const accessible = () => (
+  <Example borderColor="#000" textColor="#000" backgroundColor="#faf0e6" />
+);
+export const inaccessible = () => (
+  <Example borderColor="#000" textColor="#fff" backgroundColor="#ffff00" />
+);
+export const inaccessibleInput = () => (
+  <form>
+    <input type="text" id="name" name="name" required />
+  </form>
+);
