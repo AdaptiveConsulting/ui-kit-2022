313ddcbe650c87aa0a575c97bf4f26a4cce4b319
sky-lu <honglin.lu.sr@gmail.com>
2022-10-07 11:15:46 -0400

    style: style TextStyles component

--- src/components/Typography/TextStyles.tsx
+++ src/components/Typography/TextStyles.tsx
@@ -1,4 +1,4 @@
-import { Typography } from '@mui/material';
+import { ThemeProvider } from '@mui/material';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -6,6 +6,10 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import { padding } from '@mui/system';
+
+import { Typography } from '../../index';
+import { light } from '../../theme/index';
 
 function createData(
   category: string,
@@ -33,61 +37,63 @@ const rows = [
 ];
 export default function TextStyles() {
   return (
-    <TableContainer component={Paper}>
-      <Table
-        sx={{ width: 732, ml: 10, mt: 5, mb: 44 }}
-        size="small"
-        aria-label="a dense table"
-      >
-        <TableHead>
-          <TableRow>
-            <TableCell>
-              <Typography>Category</Typography>
-            </TableCell>
-            <TableCell align="left">
-              <Typography>Style Name</Typography>
-            </TableCell>
-            <TableCell align="left">
-              <Typography>Font Styles</Typography>
-            </TableCell>
-            <TableCell align="left">
-              <Typography>Alignment</Typography>
-            </TableCell>
-            <TableCell align="left">
-              <Typography>Size</Typography>
-            </TableCell>
-            <TableCell align="left">
-              <Typography>Case</Typography>
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.category}>
-              <TableCell component="th" scope="row">
-                <Typography variant={row.category.toLowerCase()}>
-                  {row.category}
-                </Typography>
+    <ThemeProvider theme={light}>
+      <TableContainer component={Paper}>
+        <Table
+          sx={{ width: 732, ml: 10, mt: 5, mb: 44 }}
+          size="small"
+          aria-label="a dense table"
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell sx={{ width: 122, height: 40, padding: 0 }}>
+                <Typography variant="body1">Category</Typography>
               </TableCell>
-              <TableCell align="left">
-                <Typography>{row.styleName}</Typography>
+              <TableCell align="left" sx={{ width: 122, height: 40, padding: 0 }}>
+                <Typography variant="body1">Style Name</Typography>
               </TableCell>
-              <TableCell align="left">
-                <Typography>{row.fontStyles}</Typography>
+              <TableCell align="left" sx={{ width: 122, height: 40, padding: 0 }}>
+                <Typography variant="body1">Font Styles</Typography>
               </TableCell>
-              <TableCell align="left">
-                <Typography>{row.alignment}</Typography>
+              <TableCell align="left" sx={{ width: 122, height: 40, padding: 0 }}>
+                <Typography variant="body1">Alignment</Typography>
               </TableCell>
-              <TableCell align="left">
-                <Typography>{row.size}</Typography>
+              <TableCell align="left" sx={{ width: 122, height: 40, padding: 0 }}>
+                <Typography variant="body1">Size</Typography>
               </TableCell>
-              <TableCell align="left">
-                <Typography>{row.capitalization}</Typography>
+              <TableCell align="left" sx={{ width: 122, height: 40, padding: 0 }}>
+                <Typography variant="body1">Case</Typography>
               </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.category}>
+                <TableCell component="th" scope="row" sx={{ padding: 0 }}>
+                  <Typography variant={row.category.toLowerCase()}>
+                    {row.category}
+                  </Typography>
+                </TableCell>
+                <TableCell align="left" sx={{ padding: 0, height: 40 }}>
+                  <Typography variant="body2">{row.styleName}</Typography>
+                </TableCell>
+                <TableCell align="left" sx={{ padding: 0, height: 40 }}>
+                  <Typography variant="body2">{row.fontStyles}</Typography>
+                </TableCell>
+                <TableCell align="left" sx={{ padding: 0, height: 40 }}>
+                  <Typography variant="body2">{row.alignment}</Typography>
+                </TableCell>
+                <TableCell align="left" sx={{ padding: 0, height: 40 }}>
+                  <Typography variant="body2">{row.size}</Typography>
+                </TableCell>
+                <TableCell align="left" sx={{ padding: 0, height: 40 }}>
+                  <Typography variant="body2">{row.capitalization}</Typography>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </ThemeProvider>
   );
 }
