fdf89ce8b60091b9a534db29276a67837b727b18
Wenfeng Xing <wenfeng.xing@weareadaptivee.com>
2022-10-07 10:26:43 -0400

    fix: conflict

--- src/components/Typography/index.ts
+++ src/components/Typography/index.ts
@@ -1,3 +1,18 @@
+import Typography from '@mui/material/Typography';
+
 import TypesPage from './types-page';
 
 export default TypesPage;
+
+export { Typography };
+// Update the Typography's variant prop options
+declare module '@mui/material/Typography' {
+  interface TypographyPropsVariantOverrides {
+    subheader1: true;
+    subtitle1: false;
+    subheader2: true;
+    subtitle2: false;
+    subheader3: true;
+    subheader4: true;
+  }
+}
