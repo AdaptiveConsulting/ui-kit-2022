7a6211ae2c31958c8c742130400b97b7d82e5362
sky-lu <honglin.lu.sr@gmail.com>
2022-10-13 14:51:04 -0400

    refactor: define common color for both themes

--- src/theme/palette.ts
+++ src/theme/palette.ts
@@ -19,10 +19,8 @@ declare module '@mui/material/styles/createPalette' {
   }
 }
 
-// TODO: Define MUI palettes for light theme
-export const light: PaletteOptions = {
-  mode: 'light',
-  divider: alpha('#383838', 0.2), // Todo: Import base color (not alpha) from tokens file (Grey 900)
+// common color values for both themes
+const BASIC_COLOR: PaletteOptions = {
   common: {
     black: '#000',
     white: '#FFF',
@@ -78,6 +76,13 @@ export const light: PaletteOptions = {
     A400: '#BBBBBB',
     A700: '#555555',
   },
+};
+
+// TODO: Define MUI palettes for light theme
+export const light: PaletteOptions = {
+  mode: 'light',
+  ...BASIC_COLOR,
+  divider: alpha('#383838', 0.2), // Todo: Import base color (not alpha) from tokens file (Grey 900)
   contrastThreshold: 3,
   tonalOffset: 2,
   text: {
@@ -96,62 +101,8 @@ export const light: PaletteOptions = {
 // TODO: Define MUI palettes for dark theme
 export const dark: PaletteOptions = {
   mode: 'dark',
+  ...BASIC_COLOR,
   divider: alpha('#F9F9F9', 0.2), // Todo: Import base color (not alpha) from tokens file (Grey 50)
-  common: {
-    black: '#000',
-    white: '#FFF',
-  },
-  primary: {
-    main: '#A9ABD1',
-    dark: '#5F618D',
-    light: '#DBDDFF',
-  },
-  secondary: {
-    main: '#2D96FF',
-    dark: '#005195',
-    light: '#76C6FF',
-  },
-  error: {
-    main: '#FF274B',
-    dark: '#B60017',
-    light: '#FF6877',
-  },
-  warning: {
-    main: '#FF8D00',
-    dark: '#EA4F01',
-    light: '#FFBE45',
-  },
-  success: {
-    main: '#01C38D',
-    dark: '#01603D',
-    light: '#5DF7BD',
-  },
-  buy: {
-    main: '#2D96FF',
-    dark: '#005195',
-    light: '#76C6FF',
-  },
-  sell: {
-    main: '#FF274B',
-    dark: '#B60017',
-    light: '#FF6877',
-  },
-  grey: {
-    50: '#F9F9F9',
-    100: '#F2F2F2',
-    200: '#E4E4E4',
-    300: '#CFCFCF',
-    400: '#BBBBBB',
-    500: '#7F7F7F',
-    600: '#6B6B6B',
-    700: '#555555',
-    800: '#4D4D4D',
-    900: '#383838',
-    A100: '#F2F2F2',
-    A200: '#E4E4E4',
-    A400: '#BBBBBB',
-    A700: '#555555',
-  },
   contrastThreshold: 3,
   tonalOffset: 2,
   text: {
