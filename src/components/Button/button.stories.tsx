07dfcc99d07519682edd3c03269eb425674bb234
Wenfeng Xing <wenfeng.xing@weareadaptivee.com>
2022-10-13 18:33:28 -0400

    fix: recover the file

--- src/components/Button/button.stories.tsx
+++ src/components/Button/button.stories.tsx
@@ -6,14 +6,40 @@ import ButtonDefault from './button';
 export default {
   title: 'Components/Button',
   component: ButtonDefault,
+  argTypes: {
+    variant: {
+      options: ['PRIMARY', 'SECONDARY', 'TERTIARY'],
+      control: { type: 'radio' },
+    },
+  },
 } as ComponentMeta<typeof ButtonDefault>;
 
 const Template: ComponentStory<typeof ButtonDefault> = (args) => (
-  <ButtonDefault {...args} />
+  <ButtonDefault {...args}></ButtonDefault>
 );
 
-export const based = Template.bind({});
+export const Basic = Template.bind({});
 
-based.args = {
+Basic.args = {
+  variant: 'PRIMARY',
   children: 'Hello Adaptive',
+  // click: () => alert('This is a text button'),
+};
+
+export const LeftIcon = Template.bind({});
+
+LeftIcon.args = {
+  variant: 'PRIMARY',
+  children: 'Hello Adaptive',
+  icon: 'left',
+  // click: () => alert('This is a text button'),
+};
+
+export const RightIcon = Template.bind({});
+
+RightIcon.args = {
+  variant: 'PRIMARY',
+  children: 'Hello Adaptive',
+  icon: 'right',
+  // click: () => alert('This is a text button'),
 };
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,16 +1,58 @@
-import { Button } from '@mui/material';
+import { Button, SvgIcon } from '@mui/material';
 import * as React from 'react';
 
+import { ArrowLeft, ArrowRight } from '../../icons';
 type VariantType = 'PRIMARY' | 'SECONDARY' | 'TERTIARY';
 
 interface Props {
-  children: JSX.Element;
-  variant: VariantType;
+  children?: JSX.Element;
+  variant?: VariantType;
+  disabled?: boolean;
+  icon: 'left' | 'right' | undefined;
+  click: () => void;
 }
 
-const ButtonDefault: React.FC<Props> = ({ variant = 'PRIMARY', children }) => {
+const ButtonDefault: React.FC<Props> = ({
+  variant = 'PRIMARY',
+  disabled = false,
+  children = '',
+  icon,
+  click,
+}) => {
+  if (icon === 'left') {
+    return (
+      <Button
+        disableRipple
+        variant={variant}
+        disabled={disabled}
+        onClick={click}
+        startIcon={<SvgIcon component={ArrowLeft} viewBox="-2 -4 15 25" />}
+      >
+        {children}
+      </Button>
+    );
+  } else if (icon === 'right') {
+    return (
+      <Button
+        disableRipple
+        variant={variant}
+        disabled={disabled}
+        onClick={click}
+        endIcon={
+          <SvgIcon
+            component={ArrowRight}
+            viewBox="2 -4 15 25"
+            sx={{ textAlign: 'center' }}
+          />
+        }
+      >
+        {children}
+      </Button>
+    );
+  }
+
   return (
-    <Button variant={variant} color="error" sx={{ borderRadius: '1rem' }}>
+    <Button disableRipple variant={variant} disabled={disabled} onClick={click}>
       {children}
     </Button>
   );
