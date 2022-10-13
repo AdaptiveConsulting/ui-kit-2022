4cd78fae2f583e00532fd4de706fef3bc0d7643e
Wenfeng Xing <wenfeng.xing@weareadaptivee.com>
2022-10-13 18:38:57 -0400

    fix: conflict

--- src/theme/components.ts
+++ src/theme/components.ts
@@ -6,19 +6,36 @@ const COMPONENTS = {
       {
         props: { variant: 'PRIMARY' as VariantType },
         style: {
-          border: `15px`,
+          border: `4px`,
+          borderRadius: '100px',
+          color: '#FFFFFF',
+          backgroundColor: '#AAABD1',
+          fontSize: '11px',
+          padding: '4px 4px',
+          '&:hover': {
+            backgroundColor: '#6B6B8E',
+          },
+          '&:active': {
+            border: '1px solid #6B6B8E',
+          },
+          '&:focus': {
+            border: '2px solid #FFFFFF',
+            outline: '3px solid #FF8D00',
+          },
         },
       },
       {
         props: { variant: 'SECONDARY' as VariantType },
         style: {
           border: `15px`,
+          borderRadius: '15px',
         },
       },
       {
         props: { variant: 'TERTIARY' as VariantType },
         style: {
           border: '15px',
+          borderRadius: '15px',
         },
       },
     ],
