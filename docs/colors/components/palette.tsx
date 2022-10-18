49e84d53bb27c8a249328a041b93fa8330fd513c
Wenfeng Xing <wenfeng.xing@weareadaptivee.com>
2022-10-18 11:24:15 -0400

    chore: add the first paragraph

--- docs/colors/components/palette.tsx
+++ docs/colors/components/palette.tsx
@@ -20,7 +20,29 @@ const title = (
 const Palette: React.FC = () => {
   return (
     <Grid>
-      <TopBar title={title} />
+      <Grid item xs={12}>
+        <TopBar title={title} />
+      </Grid>
+      <Grid container sx={{ bgcolor: '#323232', color: '#FFFFFF', padding: '1rem' }}>
+        <Grid item xs={12}>
+          <Typography sx={{ margin: '6px 0px' }}>Purpose</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body2" sx={{ width: '50%', margin: '6px 0px' }}>
+            Colors communicate branding. When applied consistently , they can communicate
+            information about system states, and draw attention to certain parts of the
+            screen, like an error that needs attention.
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body2" sx={{ width: '50%', margin: '6px 0px' }}>
+            Though color does much to make a design more efficient and effective, we must
+            still pursue designs that are effective even to those with little or no color
+            perception. Therefore, colors should be used in an accessible manner, and no
+            information should be communicated by color alone.
+          </Typography>
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
