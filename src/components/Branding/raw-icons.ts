687becdac54ac4cb101a20018d37fcc29750aedf
CircleCI Deploy Script <edward.prado@weareadaptive.com>
2022-10-20 11:08:51 -0400

    refactor: load LogoText's .svg file as React component

--- src/components/Branding/raw-icons.ts
+++ src/components/Branding/raw-icons.ts
@@ -1,3 +1,4 @@
 export { ReactComponent as LogoLarge } from './images/logo_large.svg';
 export { ReactComponent as LogoMaximized } from './images/logo_maximized.svg';
 export { ReactComponent as LogoSmall } from './images/logo_small.svg';
+export { ReactComponent as LogoText } from './images/logo_text.svg';
