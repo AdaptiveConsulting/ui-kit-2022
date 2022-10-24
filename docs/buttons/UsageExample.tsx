e92be866f33419319e1a068440cdae6c4aa89384
sky-lu <honglin.lu.sr@gmail.com>
2022-10-24 10:22:58 -0400

    fix conflict

---
+++ docs/buttons/UsageExample.tsx
@@ -0,0 +1,58 @@
+import { ThemeProvider } from '@emotion/react';
+import { Box, useTheme } from '@mui/material';
+
+import { Button } from '../../src/components/Button';
+import { Typography } from '../../src/index';
+import { dark } from '../../src/theme/index';
+
+interface Props {
+  title?: 'Dark' | 'Light';
+}
+
+const UsageExample: React.FC<Props> = ({ title }) => {
+  const theme = useTheme();
+  const ButtonGroup = () => {
+    if (title === 'Dark') {
+      return (
+        <ThemeProvider theme={dark}>
+          <Button variant="PRIMARY">BUTTON TEXT</Button>
+          <Button variant="SECONDARY">BUTTON TEXT</Button>
+          <Button variant="TERTIARY">BUTTON TEXT</Button>
+        </ThemeProvider>
+      );
+    } else {
+      return (
+        <>
+          <Button variant="PRIMARY">BUTTON TEXT</Button>
+          <Button variant="SECONDARY">BUTTON TEXT</Button>
+          <Button variant="TERTIARY">BUTTON TEXT</Button>
+        </>
+      );
+    }
+  };
+  return (
+    <Box>
+      <Box
+        sx={{ marginBottom: '1rem', marginLeft: '1rem', color: theme.palette.grey[50] }}
+      >
+        <Typography variant="subheader3">{title} Background </Typography>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          width: '161px',
+          height: '174px',
+          backgroundColor:
+            title === 'Dark' ? theme.palette.grey[900] : theme.palette.grey[50],
+          justifyContent: 'space-around',
+          alignItems: 'center',
+        }}
+      >
+        <ButtonGroup />
+      </Box>
+    </Box>
+  );
+};
+
+export default UsageExample;
---
+++ docs/buttons/UsagePage.tsx
@@ -0,0 +1,126 @@
+import {
+  Box,
+  Table,
+  TableBody,
+  TableCell,
+  tableCellClasses,
+  TableContainer,
+  TableHead,
+  TableRow,
+  useTheme,
+} from '@mui/material';
+
+import { Typography } from '../../src/index';
+import UsageExample from './UsageExample';
+
+function createData(variant: string, purpose: string) {
+  return { variant, purpose };
+}
+const rows = [
+  createData(
+    'Primary',
+    'This button is primarily used for the main call to' +
+      'action on the page. This button moves a user forward through the main workflow.',
+  ),
+  createData(
+    'Secondary',
+    'This button is for secondary action. A secondary action' +
+      'is an alternate option a user can take. When used as a part of a pair, the secondary button’s function is to perform another action of the set, such as “Cancel” or “Back”.',
+  ),
+  createData(
+    'Tertiary',
+    'This button is for less important actions and can sometimes stand alone. Tertiary buttons can be used in by themselves or paired with a primary button when there is more than 2 calls to action. Tertiary buttons can also be used for sub-tasks on a page where a primary button for the main and final action is present.',
+  ),
+];
+
+const UsagePage = () => {
+  const theme = useTheme();
+
+  return (
+    <Box
+      sx={{
+        width: '1060px',
+        height: '295px',
+        backgroundColor: '#323232',
+        paddingTop: '1.875rem',
+        paddingLeft: '1.875rem',
+        display: 'flex',
+        //justifyContent: 'space-around',
+      }}
+    >
+      <TableContainer
+        sx={{
+          backgroundColor: '#323232',
+          width: '674px',
+          marginRight: '0.8125rem',
+        }}
+      >
+        <Table
+          sx={{
+            [`& .${tableCellClasses.root}`]: {
+              borderBottom: 'none',
+            },
+          }}
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell
+                align="left"
+                style={{ verticalAlign: 'top' }}
+                sx={{ color: theme.palette.grey[50], padding: 0, width: '164px' }}
+              >
+                <Typography variant="subheader1">Variant</Typography>
+              </TableCell>
+              <TableCell
+                align="left"
+                sx={{
+                  color: theme.palette.grey[50],
+                  padding: 0,
+                  paddingBottom: '1.25rem',
+                  width: '510px',
+                }}
+              >
+                <Typography variant="subheader1">Purpose</Typography>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.variant}>
+                <TableCell
+                  component="th"
+                  scope="row"
+                  style={{ verticalAlign: 'top' }}
+                  sx={{
+                    color: theme.palette.grey[50],
+                    padding: 0,
+                    width: '164px',
+                  }}
+                >
+                  <Typography variant="subheader3">{row.variant}</Typography>
+                </TableCell>
+                <TableCell
+                  align="left"
+                  sx={{
+                    color: theme.palette.grey[50],
+                    padding: 0,
+                    width: '510px',
+                    paddingBottom: '1.25rem',
+                  }}
+                >
+                  <Typography variant="body2">{row.purpose}</Typography>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <UsageExample title="Dark" />
+      <Box sx={{ marginLeft: '0.56rem' }}>
+        <UsageExample title="Light" />
+      </Box>
+    </Box>
+  );
+};
+
+export default UsagePage;
