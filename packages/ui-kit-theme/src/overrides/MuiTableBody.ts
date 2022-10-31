395cddcb07b8a9b224caee0aa9c4e3694400774b
CircleCI Deploy Script <edward.prado@weareadaptive.com>
2022-10-31 09:25:55 -0400

    feat: add divider color

--- packages/ui-kit-theme/src/overrides/MuiTableBody.ts
+++ packages/ui-kit-theme/src/overrides/MuiTableBody.ts
@@ -2,7 +2,10 @@ export default {
   styleOverrides: {
     root: ({ theme }: any) => ({
       'tr:last-child td, tr:last-child th': { border: 0 },
-      'td, th': { verticalAlign: 'top' },
+      'td, th': {
+        verticalAlign: 'top',
+        borderColor: theme.palette.divider,
+      },
       th: { fontWeight: 500 },
       td: { color: theme.palette.text.secondary },
     }),
