41bd96369bf841a97cc73a4d6d85a13896eeb4fa
Wenfeng Xing <wenfeng.xing@weareadaptivee.com>
2022-12-05 11:13:50 -0500

    chore: add the responsiveness

--- docs/src/design-system/loader-spinner/LoaderSpinner.tsx
+++ docs/src/design-system/loader-spinner/LoaderSpinner.tsx
@@ -1,15 +1,17 @@
 import {
   Grid,
+  Box,
   LinearProgress as MuiLinearProgress,
   Typography,
   useTheme,
+  useMediaQuery,
 } from '@mui/material';
 
 import { TopBar } from '../building-blocks/common';
 
 const LoaderSpinner: React.FC = () => {
-  const { palette } = useTheme();
-
+  const theme = useTheme();
+  const matchesSm = useMediaQuery(theme.breakpoints.up('sm'));
   return (
     <Grid container>
       <Grid item xs={12}>
@@ -20,7 +22,7 @@ const LoaderSpinner: React.FC = () => {
           <Typography variant="h2">Usage</Typography>
         </Grid>
         <Grid container item xs={12}>
-          <Grid item xs={6}>
+          <Grid item xs={12} md={8} lg={6} xl={4}>
             <Typography>
               Loading bars notify to the user that their request is being processed.
               Although they do not provide details about what is occurring on the
@@ -33,58 +35,77 @@ const LoaderSpinner: React.FC = () => {
         </Grid>
         <Grid container item xs={12} spacing={4}>
           <Grid container item xs={12}>
-            <Grid container item xs={6}>
-              <Grid item xs={4}>
+            <Grid container item xs={12} md={8} lg={6} spacing={4}>
+              <Grid item sm={4} xs={12}>
                 <Typography>Loading bar before load begins</Typography>
               </Grid>
-              <Grid item xs={8}>
-                <Grid item xs={8}>
+              <Grid
+                item
+                sm={8}
+                xs={12}
+                sx={{ display: 'flex', alignItems: 'center', justifyContent: 'start' }}
+              >
+                <Box sx={{ width: matchesSm ? '400px' : '280px' }}>
                   <MuiLinearProgress color="inherit" variant="determinate" value={2} />
-                </Grid>
+                </Box>
               </Grid>
             </Grid>
           </Grid>
           <Grid container item xs={12}>
-            <Grid container item xs={6}>
-              <Grid item xs={4}>
+            <Grid container item xs={12} md={8} lg={6} spacing={4}>
+              <Grid item sm={4} xs={12}>
                 <Typography>Loading bar while loading</Typography>
               </Grid>
-              <Grid item xs={8}>
-                <Grid item xs={8}>
+              <Grid
+                item
+                sm={8}
+                xs={12}
+                sx={{ display: 'flex', alignItems: 'center', justifyContent: 'start' }}
+              >
+                <Box sx={{ width: matchesSm ? '400px' : '280px' }}>
                   <MuiLinearProgress color="inherit" variant="determinate" value={50} />
-                </Grid>
+                </Box>
               </Grid>
             </Grid>
           </Grid>
           <Grid container item xs={12}>
-            <Grid container item xs={6}>
-              <Grid item xs={4}>
+            <Grid container item xs={12} md={8} lg={6} spacing={4}>
+              <Grid item sm={4} xs={12}>
                 <Typography>Loading bar completed</Typography>
               </Grid>
-              <Grid container item xs={8}>
-                <Grid item xs={8}>
+              <Grid
+                item
+                sm={8}
+                xs={12}
+                sx={{ display: 'flex', alignItems: 'center', justifyContent: 'start' }}
+              >
+                <Box sx={{ width: matchesSm ? '400px' : '280px' }}>
                   <MuiLinearProgress color="inherit" variant="determinate" value={100} />
-                </Grid>
+                </Box>
               </Grid>
             </Grid>
           </Grid>
         </Grid>
-        <Grid container item xs={6}>
-          <Grid container item xs={6} sx={{ padding: '1rem 5rem 1rem 0rem' }}>
-            <Grid item xs={12}>
-              <Typography variant="h2">
-                {palette.mode === 'light' ? 'Light Mode' : 'Dark Mode'}
-              </Typography>
-            </Grid>
-            <Grid item xs={12} sx={{ padding: '1rem 0rem' }}>
+        <Grid container item xs={12} sx={{ padding: '1rem 5rem 1rem 0rem' }} spacing={6}>
+          <Grid item xs={12}>
+            <Typography variant="h2">
+              {theme.palette.mode === 'light' ? 'Light Mode' : 'Dark Mode'}
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Box sx={{ width: matchesSm ? '400px' : '280px' }}>
               <MuiLinearProgress color="inherit" variant="determinate" value={2} />
-            </Grid>
-            <Grid item xs={12} sx={{ padding: '1rem 0rem' }}>
+            </Box>
+          </Grid>
+          <Grid item xs={12}>
+            <Box sx={{ width: matchesSm ? '400px' : '280px' }}>
               <MuiLinearProgress color="inherit" variant="determinate" value={50} />
-            </Grid>
-            <Grid item xs={12} sx={{ padding: '1rem 0rem' }}>
+            </Box>
+          </Grid>
+          <Grid item xs={12}>
+            <Box sx={{ width: matchesSm ? '400px' : '280px' }}>
               <MuiLinearProgress color="inherit" variant="determinate" value={100} />
-            </Grid>
+            </Box>
           </Grid>
         </Grid>
       </Grid>
