3ef3909431674d3d9fa925ba4531d21ad6eca6b1
Wenfeng Xing <wenfeng.xing@weareadaptivee.com>
2022-12-06 14:33:11 -0500

    chore: remove unused config files

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-    "^.+\\.svg$": "jest-transform-stub"
-  },
-
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-};
