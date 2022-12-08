9dbe6982aa4622e353b90dfeac1468b20cae3607
Ian Dilling <ian.dilling@weareadaptive.com>
2022-12-08 12:59:40 -0500

    Added Graph loading state

--- packages/ui-kit-components/src/components/Typography/Typography.tsx
+++ packages/ui-kit-components/src/components/Typography/Typography.tsx
@@ -19,31 +19,35 @@ const variantMapping = {
 };
 
 const loadingFrames = keyframes`
-0% {
-  background-position: 0% 50%
-},
-50% {
-  background-position: 100% 50%
-},
-100% {
-  background-position: 0% 50%
-}
+  0% {
+    background-position: 0% 50%
+  },
+  50% {
+    background-position: 100% 50%
+  },
+  100% {
+    background-position: 0% 50%
+  }
 `;
 
-const loadingStyles = (theme: Theme) => ({
-  '&:before': {
-    content: '"-"',
-    visibility: 'hidden',
-  },
-  width: '100%',
-  borderRadius: '2px',
-  background: `linear-gradient(90deg, rgba(0, 0, 0, 0.2) 0%, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0.2) 100%)`,
-  backgroundSize: '200% 100%',
-  animation: `${loadingFrames} 2s infinite linear`,
-});
+const loadingStyles = (theme: Theme) => {
+  const gradientRgb = ({ light: '0', dark: '255' }[theme.palette.mode] + ', ').repeat(3);
+  return {
+    '&:before': {
+      content: '"-"',
+      visibility: 'hidden',
+    },
+    width: '100%',
+    borderRadius: '2px',
+    background: `linear-gradient(90deg, rgba(${gradientRgb} 0.2) 0%, rgba(${gradientRgb} 0) 50%, rgba(${gradientRgb} 0.2) 100%)`,
+    backgroundSize: '200% 100%',
+    animation: `${loadingFrames} 2s infinite linear`,
+    opacity: 0.2,
+  };
+};
 
 export interface TypographyProps extends MuiTypographyProps {
-  loading: boolean;
+  loading?: boolean;
 }
 
 const Typography = ({ loading, sx, children, ...props }: TypographyProps) => {
