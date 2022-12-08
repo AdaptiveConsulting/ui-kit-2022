33669a2760fcf4af742cc5ab418c31f00601fe37
Wenfeng Xing <wenfeng.xing@weareadaptivee.com>
2022-12-08 15:31:31 -0500

    fix: lint

--- packages/ui-kit-components/src/components/Branding/Logo/Logo.test.tsx
+++ packages/ui-kit-components/src/components/Branding/Logo/Logo.test.tsx
@@ -4,53 +4,53 @@ import { render, screen } from '@testing-library/react';
 
 import Logo from './Logo';
 
-jest.mock("../branding", () => ({
+jest.mock('../branding', () => ({
   __esModule: true,
   LogoSmall: () => {
     return <p>Small Logo</p>;
   },
   LogoLarge: () => {
-    return <p>Large Logo</p>
+    return <p>Large Logo</p>;
   },
   LogoMaximized: () => {
-    return <p>Maximized Logo</p>
-  }
+    return <p>Maximized Logo</p>;
+  },
 }));
 
-describe("Logo component test", () => {
-  describe("Given a default Logo", () => {
+describe('Logo component test', () => {
+  describe('Given a default Logo', () => {
     beforeEach(() => {
-      render(<Logo />)
-    })
+      render(<Logo />);
+    });
 
-    describe("When render the default Logo", () => {
-      it("Then should it render correctly", () => {
+    describe('When render the default Logo', () => {
+      it('Then should it render correctly', () => {
         expect(screen.getByText('Small Logo')).toBeInTheDocument();
-      })
-    })
-  })
+      });
+    });
+  });
 
-  describe("Given a large logo", () => {
+  describe('Given a large logo', () => {
     beforeEach(() => {
-      render(<Logo variant={"large"}/>)
-    })
+      render(<Logo variant={'large'} />);
+    });
 
-    describe("When render the large logo", () => {
-      it("Then should it give the large logo", () => {
+    describe('When render the large logo', () => {
+      it('Then should it give the large logo', () => {
         expect(screen.getByText('Large Logo')).toBeInTheDocument();
-      })
-    })
-  })
+      });
+    });
+  });
 
-  describe("Given a maximized logo", () => {
+  describe('Given a maximized logo', () => {
     beforeEach(() => {
-      render(<Logo variant={"maximized"}/>)
-    })
+      render(<Logo variant={'maximized'} />);
+    });
 
-    describe("When render the maximized logo", () => {
-      it("Then should it give the maximized logo", () => {
+    describe('When render the maximized logo', () => {
+      it('Then should it give the maximized logo', () => {
         expect(screen.getByText('Maximized Logo')).toBeInTheDocument();
-      })
-    })
-  })
-})
+      });
+    });
+  });
+});
--- packages/ui-kit-components/src/components/Branding/LogoBrand/LogoBrand.test.tsx
+++ packages/ui-kit-components/src/components/Branding/LogoBrand/LogoBrand.test.tsx
@@ -4,50 +4,52 @@ import { render, screen } from '@testing-library/react';
 
 import LogoBrand from './LogoBrand';
 
-jest.mock("../branding", () => ({
+jest.mock('../branding', () => ({
   __esModule: true,
-  LogoLarge: ({sx: { height = 150, width = 148 }}) => {
-    return <div data-testid={"logo-brand"}>
-      <p>{height}</p>
-      <p>{width}</p>
-    </div>
-  }
-}))
+  LogoLarge: ({ sx: { height = 150, width = 148 } }) => {
+    return (
+      <div data-testid={'logo-brand'}>
+        <p>{height}</p>
+        <p>{width}</p>
+      </div>
+    );
+  },
+}));
 
-describe("LogoBrand component tests", () => {
-  describe("Given a default LogoBrand", () => {
+describe('LogoBrand component tests', () => {
+  describe('Given a default LogoBrand', () => {
     beforeEach(() => {
-      render(<LogoBrand />)
-    })
+      render(<LogoBrand />);
+    });
 
-    it("Should be the same as the snapshots", () => {
-      expect(screen.getByTestId("logo-brand")).toMatchSnapshot();
-    })
+    it('Should be the same as the snapshots', () => {
+      expect(screen.getByTestId('logo-brand')).toMatchSnapshot();
+    });
 
-    it("Then should render the LogoBrand component correctly", () => {
-      expect(screen.getByTestId("logo-brand")).toBeInTheDocument();
-    })
+    it('Then should render the LogoBrand component correctly', () => {
+      expect(screen.getByTestId('logo-brand')).toBeInTheDocument();
+    });
 
-    it("Then should the LogoBrand component have default size", () => {
-      const element = screen.getByTestId("logo-brand");
+    it('Then should the LogoBrand component have default size', () => {
+      const element = screen.getByTestId('logo-brand');
 
-      expect(element).toHaveTextContent("150");
-      expect(element).toHaveTextContent("148");
-    })
-  })
+      expect(element).toHaveTextContent('150');
+      expect(element).toHaveTextContent('148');
+    });
+  });
 
-  describe("Given a LogoBrand with customized size", () => {
-    describe("When pass the witdh 50 and height 100", () => {
+  describe('Given a LogoBrand with customized size', () => {
+    describe('When pass the witdh 50 and height 100', () => {
       beforeEach(() => {
         render(<LogoBrand width={50} height={100} />);
-      })
+      });
 
-      it("Then should get the witdh and height displayed", () => {
-        const element = screen.getByTestId("logo-brand");
+      it('Then should get the witdh and height displayed', () => {
+        const element = screen.getByTestId('logo-brand');
 
-        expect(element).toHaveTextContent("50");
-        expect(element).toHaveTextContent("100");
-      })
-    })
-  })
-})
+        expect(element).toHaveTextContent('50');
+        expect(element).toHaveTextContent('100');
+      });
+    });
+  });
+});
